cd5ac5c1e6678952aac95365aed84f37abd3bd68
wungthing <wungthingkashungnao123@gmail.com>
2025-04-24 23:05:42 +0530

    login added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import styles from "./App.module.css";
 import SignUp from "./pages/signup/SignUp";
+import SignIn from "./pages/signin/SignIn";
 
 function App() {
   return (
     <div className={styles.App}>
       <Routes>
         <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
       </Routes>
     </div>
   );
---
+++ src/pages/signin/SignIn.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import styles from "./signInStyle.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import { Bounce, ToastContainer, toast } from "react-toastify";
+
+const SignIn = () => {
+  const [userDetails, setUserDetails] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    console.log("email>>>", userDetails.email);
+    console.log("password>>>", userDetails.password);
+    console.log("users>>>>", users);
+
+    for (const element of users) {
+      if (
+        element.email === userDetails.email &&
+        element.password === userDetails.password
+      ) {
+        toast.success("Successfully signed in!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          transition: Bounce,
+        });
+        navigate("/");
+      }
+    }
+
+    toast.error("Invalid email or password!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      transition: Bounce,
+    });
+    return;
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2>Log In</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="email"
+          placeholder="example@mail.com"
+          value={userDetails.email}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, email: e.target.value })
+          }
+        />
+
+        <input
+          required
+          type="password"
+          placeholder="Password"
+          value={userDetails.password}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, password: e.target.value })
+          }
+        />
+
+        <button>Login</button>
+        <div className={styles.link}>
+          <p>
+            Don't have an account?{" "}
+            <Link to={"/signup"} className={styles.singup}>
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default SignIn;
